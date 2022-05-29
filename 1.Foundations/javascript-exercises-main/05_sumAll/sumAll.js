f5f71e1a8f37d0f2c850544160f4da7bdbf72822
Nikola <nikola@email.com>
2022-05-29 19:51:13 +0300

    exercise 5 completed

--- 1.Foundations/javascript-exercises-main/05_sumAll/sumAll.js
+++ 1.Foundations/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  if (typeof startNum !== 'number'
+    || typeof endNum !== 'number'
+    || startNum < 0
+    || endNum < 0) {
+    return 'ERROR';
+  }
 
+  let sum = 0;
+
+  for (let i = Math.min(startNum, endNum); i <= Math.max(startNum, endNum); i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
