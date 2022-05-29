19542178e35b27f445a217af5c1e3ede8f529497
Nikola <nikola@email.com>
2022-05-29 19:42:31 +0300

    exercise 4 completed

--- 1.Foundations/javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ 1.Foundations/javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  const arr = args[0];
+  const itemsToRemove = args.slice(1);
 
+  itemsToRemove.forEach(el => {
+    const index = arr.findIndex(e => e === el);
+    
+    if(index > -1) {
+      arr.splice(index, 1);
+    }
+  });
+
+  return arr;
 };
 
 // Do not edit below this line
