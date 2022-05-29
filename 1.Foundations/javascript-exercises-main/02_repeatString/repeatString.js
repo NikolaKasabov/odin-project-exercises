860b66eee30ebe9a0f088f9e336548a38b7c4061
Nikola <nikola@email.com>
2022-05-29 15:43:02 +0300

    exercise 02 completed

--- 1.Foundations/javascript-exercises-main/02_repeatString/repeatString.js
+++ 1.Foundations/javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
+const repeatString = function(str, repeatTimes) {
+  if(repeatTimes < 0) return 'ERROR';
 
+  return str.repeat(repeatTimes);
 };
 
 // Do not edit below this line
