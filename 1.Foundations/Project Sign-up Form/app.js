4a79b91783918ab632309aef3c06609a1c4ceb76
Nikola <nikola@email.com>
2022-05-31 17:28:16 +0300

    sign-up form complete

--- 1.Foundations/Project Sign-up Form/app.js
+++ 1.Foundations/Project Sign-up Form/app.js
@@ -2,6 +2,27 @@ const passwordEl = document.getElementById('password');
 const confirmPasswordEl = document.getElementById('confirm-password');
 const form = document.querySelector('form');
 
-form.addEventListener('submit', () => {
-  console.log(form);
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  removeErrorMessage();
+
+  if (!arePasswordsTheSame()) {
+    addErrorMessage();
+  }
 });
+
+function arePasswordsTheSame() {
+  const password = passwordEl.value;
+  const confirmPassword = confirmPasswordEl.value;
+  return password === confirmPassword;
+}
+
+function addErrorMessage() {
+  passwordEl.parentElement.classList.add('error');
+  confirmPasswordEl.parentElement.classList.add('error');
+}
+
+function removeErrorMessage() {
+  passwordEl.parentElement.classList.remove('error');
+  confirmPasswordEl.parentElement.classList.remove('error');
+}
