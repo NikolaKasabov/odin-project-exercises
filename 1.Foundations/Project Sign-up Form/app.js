acf619238d3e95d639391ab42e10b0037caab3ac
Nikola <nikola@email.com>
2022-05-31 16:41:03 +0300

    add js file

---
+++ 1.Foundations/Project Sign-up Form/app.js
@@ -0,0 +1,7 @@
+const passwordEl = document.getElementById('password');
+const confirmPasswordEl = document.getElementById('confirm-password');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', () => {
+  console.log(form);
+});
