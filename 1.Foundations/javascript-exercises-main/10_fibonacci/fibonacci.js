e363be643e1b24dc04e10aac5ce2eb05e2dbd4ea
Nikola <nikola@email.com>
2022-06-03 15:47:43 +0300

    js exercise 10 completed

--- 1.Foundations/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ 1.Foundations/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  const num = parseInt(n);
+  const arr = [1, 1];
 
+  if (!num || num < 0) {
+    return 'OOPS';
+  } else if (num === 1 || num === 2) {
+    return 1;
+  }
+
+  for (let i = 2; i < num; i++) {
+    const currentNum = arr[i - 2] + arr[i - 1];
+    arr.push(currentNum);
+  }
+
+  return arr[arr.length - 1];
 };
 
 // Do not edit below this line
