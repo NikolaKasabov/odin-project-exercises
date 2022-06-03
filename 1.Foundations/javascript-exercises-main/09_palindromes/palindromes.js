11d022d66d405a717c6ae502b827d7278e7fb742
Nikola <nikola@email.com>
2022-06-03 15:17:31 +0300

    js exercise 9 completed

--- 1.Foundations/javascript-exercises-main/09_palindromes/palindromes.js
+++ 1.Foundations/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let onlyLetters = '';
 
+  str.split('')
+    .forEach(char => {
+      if ((char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122)) {
+        onlyLetters += char.toLowerCase();
+      }
+    });
+  
+  const reversed = onlyLetters.split('').reverse().join('');
+  
+  return onlyLetters === reversed;
 };
 
 // Do not edit below this line
